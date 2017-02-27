36b5a945a0bcafcc43df48ced4591a284bcfb5b0
Mar <marcianelli.m@att.net>
2017-02-27 10:06:10 -0800

    edits

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,89 @@
-document.ready(function()){
+$( document ).ready(function() {
+
+	Sound = new Audio('./assets/audio/Non-Stop.mp3'),
+  
+	var correctAnswers = []
+	var wrongAnswers = []
+	var unanswered = []
+	var counter = [] //not sure if i need this
 
 
-var correctAnswers = []
-var wrondAnswers = []
-var unanswered = []
-var counter = [] //not sure if i need this
+// var radios = document.getElementsByName('radio1');
+
+// for (var i = 0, length = radios.length; i < length; i++) {
+//     if (radios[i].checked) {
+//     	correctAnswers++;
+//       
+//         console.log(correctAnswers);
+//         console.log(wrongAnswers);
+// 	        console.log(wrongAnswers);
+
+        // only one radio can be logically checked, don't check the rest
+        //break;
+    
+
+
+// if ($("input[id='sizeSmall']checked").val() == "true"){
+// 	correctAnswers++;
+// } else{
+//     wrongAnswers++;
+
+// 	console.log(correctAnswers);
+// 	console.log(wrongAnswers);
+// }
+
+// var radio = document.getElementsByClassName('questionOne');
+// var value;
+// for (var i = 0; i < radio.length; i++) {
+//     if (radio[i].value === 'true' && radios[i].checked) {
+//         correctAnswers++;
+//         value = radio[i].value;       
+//     }
+
+//     console.log(correctAnswers);
+// 	console.log(wrongAnswers);
+// 	console.log("work");
+// }
+
+// if(document.getElementById("false").checked) {
+// 	correctAnswers++;
+
+// }else{
+// 	wrongAnswers++;
+
+
+// 	console.log(correctAnswers);
+// 	console.log(wrongAnswers);
+// }
+
+// 	
+
+// $('#sizeSmall').click(function() {
+//     if (!$("input[id='sizeSmall']:checked").val()) {
+//     	wrongAnswers++;
+//     	alert('wrong answer!');
+//         // return false;
+//     }
+//     else {
+//     	correctAnswers++;
+//       alert('correct answer');
+//     }
+
+// 	console.log(correctAnswers);
+// 	console.log(wrongAnswers);
+
+//   });
+
+// if (!$("input(id="sizeSmall")").is(':checked')) {
+// 	correctAnswers++;
+// 	alert('correct');
+// }
+// else {
+// 	wrongAnswers++;
+// 	alert('no!');
+// }
+// console.log(correctAnswers);
+// console.log(wrongAnswers);
 
 
 
@@ -17,4 +96,4 @@ var counter = [] //not sure if i need this
 
 
 
-}
+});
